6639b4b3bd064064306e26bc31e4873fa909244a
J-Hoogvliet <justinhoogvliet@outlook.com>
2024-05-31 11:13:14 +0200

    Werkt?

--- src/js/endOfGame.js
+++ src/js/endOfGame.js
@@ -18,5 +18,15 @@ export class EndGame extends Scene {
   onInitialize(engine) {
     const land = new EndBackground();
     this.add(land);
+
+    const text = new engine.Text({
+      text: "Press Enter",
+      font: new engine.Font({
+        family: "Arial",
+        size: 24,
+        color: Color.White,
+      }),
+    });
+    this.add(text);
   }
 }
